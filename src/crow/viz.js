6159e85ac220ca580112ac2894f484e53fd9ba38
Robey Pointer <robey@kibanlabs.com>
2015-11-09 11:46:33 -0800

    uncomment viz and hope it works as-is.

--- src/crow/viz.js
+++ src/crow/viz.js
@@ -1,59 +1,55 @@
-// "use strict";
-//
-// const path = require("path");
-// const ring = require("./ring");
-//
-// // find our static folder -> ./lib/crow/viz/viz.js -> ./static
-// const staticPath = path.resolve(require.resolve("../crow"), "../../static");
-//
-// /*
-//  * create a sub-path on your existing web server for displaying per-server
-//  * metrics:
-//  *
-//  *     var app = express();
-//  *     var metrics = new crow.Registry();
-//  *     app.use("/viz", crow.viz(express, metrics));
-//  *     app.listen(8080);
-//  *
-//  * you can place it at any path you want.
-//  */
-// function viz(express, registry, span = ring.DEFAULT_SPAN) {
-//   let router = express.Router();
-//   router.use("/", express.static(staticPath));
-//
-//   var observer = new ring.RingBufferObserver(registry, span);
-//   router.get("/history.json", (request, response) => {
-//     response.type("json");
-//     response.send(observer.toJson());
-//   });
-//
-//   router.get("/debug.json", (request, response) => {
-//     response.type("json");
-//     response.send(observer.get());
-//   });
-//
-//   router.get("/current.json", (request, response) => {
-//     const latest = observer.getLatest();
-//     response.type("json");
-//     response.send(JSON.stringify(latest));
-//   });
-//
-//   return router;
-// }
-//
-// /*
-//  * if you don't have any other use for a web server, you can use this to
-//  * do the whole creation:
-//  *
-//  *     var metrics = new crow.Registry();
-//  *     crow.startVizServer(express, metrics);
-//  */
-// function startVizServer(express, registry, port = 8080) {
-//   const app = express();
-//   app.use("/", viz(express, registry));
-//   app.listen(port);
-// }
-//
-//
-// exports.startVizServer = startVizServer;
-// exports.viz = viz;
+"use strict";
+
+import path from "path";
+import { RingBufferObserver } from "./ring";
+
+// find our static folder -> ./lib/crow/viz/viz.js -> ./static
+const staticPath = path.resolve(require.resolve("../crow"), "../../static");
+
+/*
+ * create a sub-path on your existing web server for displaying per-server
+ * metrics:
+ *
+ *     const app = express();
+ *     const metrics = new crow.Registry();
+ *     app.use("/viz", crow.viz(express, metrics));
+ *     app.listen(8080);
+ *
+ * you can place it at any path you want.
+ */
+export function viz(express, registry, span) {
+  const router = express.Router();
+  router.use("/", express.static(staticPath));
+
+  const observer = new RingBufferObserver(registry, span);
+  router.get("/history.json", (request, response) => {
+    response.type("json");
+    response.send(observer.toJson());
+  });
+
+  router.get("/debug.json", (request, response) => {
+    response.type("json");
+    response.send(observer.get());
+  });
+
+  router.get("/current.json", (request, response) => {
+    const latest = observer.getLatest().flatten();
+    response.type("json");
+    response.send(JSON.stringify(latest));
+  });
+
+  return router;
+}
+
+/*
+ * if you don't have any other use for a web server, you can use this to
+ * do the whole creation:
+ *
+ *     var metrics = new crow.Registry();
+ *     crow.startVizServer(express, metrics);
+ */
+export function startVizServer(express, registry, port = 8080) {
+  const app = express();
+  app.use("/", viz(express, registry));
+  app.listen(port);
+}
