8bd854c56de0e7885bf819d5dd0946935e43279b
Robey Pointer <robeypointer@gmail.com>
2015-11-15 14:55:24 -0800

    add "rank" option to deltaObserver, to allow turning counters into distribution-by-tag.

--- src/crow/delta.js
+++ src/crow/delta.js
@@ -3,24 +3,51 @@
 import Snapshot from "./snapshot";
 
 class DeltaObserver {
-  constructor() {
+  constructor(options = {}) {
     this.previous = new Map();
+    this.rank = options.rank || [];
+    // convert 'match' into a regex, and 'tags' into a form suitable for merging.
+    this.rank.forEach(r => {
+      if (!r.match) r.match = ".*";
+      if (typeof r.match == "string") r.match = new RegExp(r.match);
+      r.mergeTags = {};
+      r.tags.forEach(t => r.mergeTags[t] = null);
+    });
   }
 
   compute(snapshot) {
     const map = new Map();
+    const newDistributions = new Set();
+
     for (const [ metric, value ] of snapshot.map) {
+      let newValue = value;
+
       if (metric.type == "counter") {
         const key = metric.name + "{" + metric.tags.canonical + "}";
         const delta = value - (this.previous.get(key) || 0);
         this.previous.set(key, value);
-        map.set(metric, delta);
-      } else {
-        // pass-thru.
-        map.set(metric, value);
+        newValue = delta;
       }
+
+      let squelch = false;
+      this.rank.forEach(r => {
+        if (r.match.test(metric.name)) {
+          squelch = true;
+          const newName = r.name || metric.name;
+          const newTags = metric.tags.merge(r.mergeTags);
+          const d = snapshot.registry.distribution(newName, newTags);
+          d.add(newValue);
+          newDistributions.add(d);
+        }
+      });
+
+      if (!squelch) map.set(metric, newValue);
     }
-    return new Snapshot(snapshot.timestamp, map);
+
+    // add any new distributions we computed.
+    for (const d of newDistributions) map.set(d, d.value);
+
+    return new Snapshot(snapshot.registry, snapshot.timestamp, map);
   }
 }
 
@@ -31,7 +58,7 @@ class DeltaObserver {
  * Pass in an observer function that will receive the delta'd snapshots as
  * they are emitted. Returns a wrapped observer that takes normal snapshots.
  */
-export default function deltaObserver(observer) {
-  const d = new DeltaObserver();
+export default function deltaObserver(observer, options) {
+  const d = new DeltaObserver(options);
   return snapshot => observer(d.compute(snapshot));
 }
--- test/src/test_delta.js
+++ test/src/test_delta.js
@@ -6,7 +6,7 @@ import "should";
 import "source-map-support/register";
 
 
-describe("DeltaObserver", () => {
+describe("deltaObserver", () => {
   it("passes through gauges and distributions unharmed", () => {
     const snapshots = [];
     const r = new MetricsRegistry();
@@ -61,5 +61,55 @@ describe("DeltaObserver", () => {
     snapshots.length.should.eql(6);
     snapshots.map(s => (s.flatten().get("cats") || {}).value).should.eql([ undefined, 1, 3, 2, 0, 0 ]);
     snapshots.map(s => (s.flatten().get("dogs") || {}).value).should.eql([ undefined, undefined, undefined, 5, 0, 1 ]);
+  });
+
+  it("will turn a counter into a distribution, by tag", () => {
+    const snapshots = [];
+    const r = new MetricsRegistry();
+    r.addObserver(deltaObserver(s => snapshots.push(s), {
+      rank: [
+        { name: "traffic_per_session", match: "bytes", tags: [ "session" ] }
+      ]
+    }));
+
+    r.counter("bytes", { session: "3" }).increment(10);
+    r.counter("bytes", { session: "4" }).increment(20);
+    r.counter("bytes", { session: "5" }).increment(30);
+    r._publish();
+    Array.from(snapshots[0].flatten()).sort().should.eql([
+      [ "traffic_per_session{p=0.5}", { type: "distribution", value: 20 } ],
+      [ "traffic_per_session{p=0.99}", { type: "distribution", value: 30 } ],
+      [ "traffic_per_session{p=0.9}", { type: "distribution", value: 30 } ],
+      [ "traffic_per_session{p=count}", { type: "distribution", value: 3 } ],
+      [ "traffic_per_session{p=sum}", { type: "distribution", value: 60 } ]
+    ]);
+  });
+
+  it("will preserve other tags when constructing distributions", () => {
+    const snapshots = [];
+    const r = new MetricsRegistry();
+    r.addObserver(deltaObserver(s => snapshots.push(s), {
+      rank: [
+        { name: "traffic_per_session", match: "bytes", tags: [ "session" ] }
+      ]
+    }));
+
+    r.counter("bytes", { type: "cat", session: "3" }).increment(10);
+    r.counter("bytes", { type: "mouse", session: "4" }).increment(20);
+    r.counter("bytes", { type: "mouse", session: "5" }).increment(29);
+    r.counter("bytes", { type: "mouse", session: "5" }).increment(1);
+    r._publish();
+    Array.from(snapshots[0].flatten()).sort().should.eql([
+      [ "traffic_per_session{p=0.5,type=cat}", { type: "distribution", value: 10 } ],
+      [ "traffic_per_session{p=0.5,type=mouse}", { type: "distribution", value: 30 } ],
+      [ "traffic_per_session{p=0.9,type=cat}", { type: "distribution", value: 10 } ],
+      [ "traffic_per_session{p=0.9,type=mouse}", { type: "distribution", value: 30 } ],
+      [ "traffic_per_session{p=0.99,type=cat}", { type: "distribution", value: 10 } ],
+      [ "traffic_per_session{p=0.99,type=mouse}", { type: "distribution", value: 30 } ],
+      [ "traffic_per_session{p=count,type=cat}", { type: "distribution", value: 1 } ],
+      [ "traffic_per_session{p=count,type=mouse}", { type: "distribution", value: 2 } ],
+      [ "traffic_per_session{p=sum,type=cat}", { type: "distribution", value: 10 } ],
+      [ "traffic_per_session{p=sum,type=mouse}", { type: "distribution", value: 50 } ]
+    ]);
   });
 });
