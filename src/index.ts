3a363d2b60bc35221f5827da2af53d3d89ccd1ce
Robey Pointer <robeypointer@gmail.com>
2017-01-22 21:14:28 -0800

    export counter, distribution, gauge interfaces.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { BiasedQuantileDistribution } from "./crow/bqdist";
 export { MetricName, MetricType } from "./crow/metric_name";
-export { Metrics } from "./crow/metrics";
+export { Counter, Distribution, Gauge, Metrics } from "./crow/metrics";
 export { MetricsRegistry, RegistryOptions } from "./crow/registry";
 export { Snapshot } from "./crow/snapshot";
 
